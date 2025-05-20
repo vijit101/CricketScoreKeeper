464e48e16d41d85fe3351d64c80e245a960bda2f
Shubham <40598861+vijit101@users.noreply.github.com>
2025-05-20 18:34:06 +0530

    This version updates the score keeper app to be able to update runs from 1 to 6 and wicket from 0-10

--- scorekeep.js
+++ scorekeep.js
@@ -8,9 +8,19 @@ let wicket = 0;
 
 
 // -- function area -- 
-function addOne() {
-  score++; // re-render updatedscore
-  ReactRoot.render(<App />);
+function addScore(num) {
+  if(wicket<10){
+    score += num; // re-render updatedscore
+    ReactRoot.render(<App />);
+  }
+}
+
+function addWicket(){
+  if(wicket <10){
+    wicket++;
+    ReactRoot.render(<App />);
+  }
+  
 }
 
 
@@ -20,12 +30,13 @@ function ButtonElementComponent()
   return (
     <>
       <div className="button">
-        <button onClick={addOne}>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>Wicket</button>
+        <button onClick={()=>{addScore(1)}}>1</button>
+        <button onClick={()=>{addScore(2)}}>2</button>
+        <button onClick={()=>{addScore(3)}}>3</button>
+        <button onClick={()=>{addScore(4)}}>4</button>
+        <button onClick={()=>{addScore(5)}}>5</button>
+        <button onClick={()=>{addScore(6)}}>6</button>
+        <button onClick={addWicket}>Wicket</button>
       </div>
     </>
   );
